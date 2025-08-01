f1de3fe9fe188ad1a812ace8a051962740d257af
lolearningcode <lolearningcode@gmail.com>
2025-08-01 11:52:01 -0700

    update to main tf bucket name

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 resource "aws_s3_bucket" "exzien_test" {
-  bucket = "exzien-drift-test-bucket-${random_id.rand.hex}"
+  bucket = "exzien-drift-test-bucket-test"
 
   tags = {
     Environment = "Test"
   }
-}
-
-resource "random_id" "rand" {
-  byte_length = 4
 }
